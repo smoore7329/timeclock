72d08bcc45ea112ca72b842a9eb4fbe880d496dd
smoore7329 <smoore@aimeautomation.com>
2026-09-19 11:51:29 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Time Clock offline cache: serves the app from cache when the network is down, updates in the background.
-const CACHE = 'timeclock-v5';
+const CACHE = 'timeclock-v9';
 const FILES = ['./', './index.html', './manifest.json', './admin.html'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
