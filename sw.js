03f37f519e26867bb0435f65e7b48d8e5ee8645e
smoore7329 <smoore@aimeautomation.com>
2026-09-23 18:07:02 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Time Clock offline cache. Network first (so updates show immediately), cache as fallback when offline.
-const CACHE = 'timeclock-v10';
+const CACHE = 'timeclock-v11';
 const FILES = ['./', './index.html', './manifest.json', './admin.html'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
